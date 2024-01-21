efce54feb76d03e53cf9a6327c3a1f7c8e78abfa
harshalDev <harshalwarade344@gmail.com>
2024-01-21 10:30:49 +0530

    pushed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser, displayMsg } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 router.route("/msg").get(displayMsg);
 
 export default router;
